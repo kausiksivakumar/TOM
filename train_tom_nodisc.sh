0e617e9bed48622e6d28d7830b89b906b80ee337
JasonMa2016 <yechengma@gmail.com>
2023-05-15 15:58:07 -0400

    project website init

--- train_tom_nodisc.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-CUDA_VISIBLE_DEVICES=1 python mbpo.py --env "Hopper-v2"      --rollout_length 1 --method "tom" --seed 1 --no_disc  --f "chi"  &
-CUDA_VISIBLE_DEVICES=1 python mbpo.py --env "Walker2d-v2"    --rollout_length 1 --method "tom" --seed 1 --no_disc  --f "chi"  &
-CUDA_VISIBLE_DEVICES=1 python mbpo.py --env "HalfCheetah-v2" --rollout_length 1 --method "tom" --seed 1 --no_disc  --f "chi"  &
-CUDA_VISIBLE_DEVICES=1 python mbpo.py --env "Humanoid-v2"    --rollout_length 1 --method "tom" --seed 1 --no_disc  --f "chi"  &
-CUDA_VISIBLE_DEVICES=0 python mbpo.py --env "Ant-v2"         --rollout_length 1 --method "tom" --seed 1 --no_disc  --f "chi" 
-
